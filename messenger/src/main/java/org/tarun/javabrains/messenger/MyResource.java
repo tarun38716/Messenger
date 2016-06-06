7120f5e2f4d9d1da98026937beaf137ed443186a
Tarun Agarwal <tatarunaggarwal@gmail.com>
2016-06-07 00:41:44 +0530

    Change Commit

--- messenger/src/main/java/org/tarun/javabrains/messenger/MyResource.java
+++ messenger/src/main/java/org/tarun/javabrains/messenger/MyResource.java
@@ -20,6 +20,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
+    	System.out.println("Change Commited");
         return "Got it!";
     }
 }
